898e59f9cbd4d4178dd1e3ee2dbebf5af468afb7
Minho Ji <59356172+minho4418@users.noreply.github.com>
2022-05-06 23:47:56 +0900

    Update SalesRepository.java

--- sp03_sales/src/main/java/com/my/sales/repository/SalesRepository.java
+++ sp03_sales/src/main/java/com/my/sales/repository/SalesRepository.java
@@ -1,23 +1,55 @@
 package com.my.sales.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.my.sales.dto.Product;
 import com.my.sales.dto.Sales;
 
 @Repository
 public class SalesRepository {
 	@Autowired
-	private SqlSession sqlSession;
-	//상품 추가
-	public int insert(Sales sales){
-		try {
-			return sqlSession.insert("com.my.sales.SalesMapper.insert", sales);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return 0; //예외발생시 0 리턴
+	private SqlSession sqlSession; 
+	
+	public int insert(Sales sales) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("com.my.sales.SalesMapper.insert", sales);
 	}
+
+	public List<Map<String, Object>> selectList(Map<String, String> findmap) {
+		
+		return sqlSession.selectList("com.my.sales.SalesMapper.selectList", findmap);
+	}
+
+	public Sales selectOne(int seq) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("com.my.sales.SalesMapper.selectOne", seq);
+	}
+
+	public int update(Sales sales) {
+		// TODO Auto-generated method stub
+		return sqlSession.update("com.my.sales.SalesMapper.update", sales);
+	}
+
+	public int delete(int seq) {
+		// TODO Auto-generated method stub
+		return sqlSession.delete("com.my.sales.SalesMapper.delete", seq);
+	}
+	
+	public List<Sales> selectList_pcode(String pcode){
+		return sqlSession.selectList("com.my.sales.SalesMapper.selectList_pcode",pcode);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
